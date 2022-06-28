5f95e1746e6a2ad93a0fb51ceb6dc21fce030107
Mateo <petragliamateo@gmail.com>
2022-06-28 12:49:22 -0300

    courseinfo 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,30 +1,4 @@
-const Header = ({ course }) => <h2>{course}</h2>
-
-const Total = ({ sum }) => <p style={{fontWeight: 'bold'}}>Total of exercises {sum}</p>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => parts.map((part) => <Part key={part.id} part={part} />)
-
-export const Course = ({ course }) => {
-  const sum = course.parts.reduce((previus, current) => {
-    const currentValue = current.exercises;
-    const previusValue = previus.exercises ? previus.exercises : previus;
-    return previusValue + currentValue;
-  })
-
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total sum={sum} />
-    </div>
-  )
-}
-
+import Course from "./Course";
 
 const App = () => {
   const courses = [
