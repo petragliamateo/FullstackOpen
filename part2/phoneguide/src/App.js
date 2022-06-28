85e467c7032a7f6b0a46fd2a7f8f98a9b651c3fe
Mateo <petragliamateo@gmail.com>
2022-06-28 20:15:59 -0300

    phoneguide 2.10

--- part2/phoneguide/src/App.js
+++ part2/phoneguide/src/App.js
@@ -27,7 +27,29 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
+      
+      <Filter setFilter={setFilter} filter={filter} />
+
+      <h3>Add a new</h3>
+      
+      <PersonForm
+        handleSubmit={handleSubmit}
+        newName={newName}
+        setNewName={setNewName}
+        newPhone={newPhone}
+        setNewPhone={setNewPhone}
+      />
+
+      <h3>Numbers</h3>
+
+      <Persons persons={persons} filter={filter} />
+    </div>
+  )
+}
+
+const Filter = ({ setFilter, filter }) => {
+  return (
+    <div>
         filter shown with:
         <input
           onChange={({ target }) => setFilter(target.value)}
@@ -35,33 +57,40 @@ const App = () => {
           name='filter'
         />
       </div>
-      <h2>Add a new</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          name:
-          <input
-            onChange={({ target }) => setNewName(target.value)}
-            value={newName}
-            name='name'
-          />
-        </div>
-        <div>
-          number:
-          <input
-            onChange={({ target }) => setNewPhone(target.value)}
-            value={newPhone}
-            name='phone'
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {persons.map((person) => person.name.toLowerCase().includes(filter.toLowerCase()) ? (
-        <p key={person.name}>{person.name} {person.number}</p>
-      ) : null)}
-    </div>
+  )
+}
+
+const PersonForm = ({ handleSubmit, newName, setNewName, newPhone, setNewPhone }) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        name:
+        <input
+          onChange={({ target }) => setNewName(target.value)}
+          value={newName}
+          name='name'
+        />
+      </div>
+      <div>
+        number:
+        <input
+          onChange={({ target }) => setNewPhone(target.value)}
+          value={newPhone}
+          name='phone'
+        />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Persons = ({ persons, filter }) => {
+  return (
+    persons.map((person) => person.name.toLowerCase().includes(filter.toLowerCase()) ? (
+      <p key={person.name}>{person.name} {person.number}</p>
+    ) : null)
   )
 }
 
