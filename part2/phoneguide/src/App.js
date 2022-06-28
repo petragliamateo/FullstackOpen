6cb97fef7d04f53c55a307e8793922436b43c745
Mateo <petragliamateo@gmail.com>
2022-06-28 20:06:29 -0300

    phoneguide 2.8

--- part2/phoneguide/src/App.js
+++ part2/phoneguide/src/App.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const [ newName, setNewName ] = useState('');
+  const [ newPhone, setNewPhone ] = useState('');
+  const [ filter, setFilter ] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -14,13 +19,23 @@ const App = () => {
       window.alert(`${newName} is alredy added to phonebook`);
       return;
     }
-    setPersons((prev) => prev.concat({ name: newName }));
+    setPersons((prev) => prev.concat({ name: newName, number: newPhone }));
     setNewName('');
+    setNewPhone('');
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with:
+        <input
+          onChange={({ target }) => setFilter(target.value)}
+          value={filter}
+          name='filter'
+        />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name:
@@ -31,11 +46,21 @@ const App = () => {
           />
         </div>
         <div>
+          number:
+          <input
+            onChange={({ target }) => setNewPhone(target.value)}
+            value={newPhone}
+            name='phone'
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => <p key={person.name}>{person.name}</p>)}
+      {persons.map((person) => person.name.toLowerCase().includes(filter.toLowerCase()) ? (
+        <p key={person.name}>{person.name} {person.number}</p>
+      ) : null)}
     </div>
   )
 }
