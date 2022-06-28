fb0a4d214b8f501a1e76c5195e31250cef195a89
Mateo <petragliamateo@gmail.com>
2022-06-27 22:01:53 -0300

    courseinfo 1.2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -26,11 +26,15 @@ const Header = ({ course }) => {
 const Content = ({ part1, part2, part3, exercises1, exercises2, exercises3 }) => {
   return (
     <>
-      <p>{part1} {exercises1}</p>
-      <p>{part2} {exercises2}</p>
-      <p>{part3} {exercises3}</p>
+      <Part part={part1} exercise={exercises1} />
+      <Part part={part2} exercise={exercises2} />
+      <Part part={part3} exercise={exercises3} />
     </>
   )
+}
+
+const Part = ({ part, exercise }) => {
+  return <p>{part} {exercise}</p>
 }
 
 const Total = ({ exercises1, exercises2, exercises3 }) => {
