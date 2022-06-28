e2b2392cb96698f53c31d7ce81932016908fdef1
Mateo <petragliamateo@gmail.com>
2022-06-28 09:50:39 -0300

    unicafe 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const total = good + neutral + bad;
+  const averange = (good - bad) / (total);
+  const positive = (good / total) * 100;
 
   return (
     <div>
@@ -20,11 +24,14 @@ const App = () => {
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {total}</p>
+        <p>averange {averange}</p>
+        <p>positive {positive} %</p>
       </div>
     </div>
-  )
+  );
 }
 
 ReactDOM.render(<App />, 
   document.getElementById('root')
-)
+);
