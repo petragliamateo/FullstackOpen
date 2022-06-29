6ee0f068a34b097c2ce0dc82a7f36f86bbbdef57
Mateo <petragliamateo@gmail.com>
2022-06-28 23:18:05 -0300

    phoneguide 2.11

--- part2/phoneguide/src/App.js
+++ part2/phoneguide/src/App.js
@@ -1,12 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('');
   const [ newPhone, setNewPhone ] = useState('');
   const [ filter, setFilter ] = useState('');
@@ -23,6 +19,15 @@ const App = () => {
     setNewName('');
     setNewPhone('');
   }
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then((response) => {
+        const data = response.data;
+        setPersons(data)
+      })
+  }, [])
+  
 
   return (
     <div>
