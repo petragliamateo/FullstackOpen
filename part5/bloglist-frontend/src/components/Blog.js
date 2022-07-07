554af0d9ea70636bf85971b5ebb3c4f565915d87
Mateo <petragliamateo@gmail.com>
2022-07-07 19:41:40 -0300

    bloglist-frontend 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,34 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false);
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  console.log(blog);
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setVisible((prev) => !prev)}>
+          {visible ? 'hide' : 'view'}
+        </button>
+      </div>
+      {visible && (
+        <div>
+          {blog.url}<br />
+          {`likes: ${blog.likes}`}
+          <button>like</button><br />
+          {blog.user.name}
+        </div>
+      )}
+    </div>  
+  )
+}
+
+export default Blog;
