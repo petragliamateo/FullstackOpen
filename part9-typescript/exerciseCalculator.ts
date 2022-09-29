2940c28a2a3a550db4c53dc23b039abc1b44bbf3
Mateo <petragliamateo@gmail.com>
2022-09-29 09:56:01 -0300

    9.2

---
+++ part9-typescript/exerciseCalculator.ts
@@ -0,0 +1,31 @@
+interface Result {
+  periodLength: number;
+  trainingDays: number;
+  target: number;
+  average: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+}
+// Entrada de datos: [3, 0, 2, 4.5, 0, 3, 1] => 3 horas el lunes, 0 el martes, 2 el miercoles, etc..
+// target es el objetivo diario, p.e. target = 2 serian 2 horas promedio diarias
+
+const exerciseCalculator = (trainned: Array<number>, target: number): Result => {
+  const periodLength = trainned.length;
+  const trainingDays = trainned.filter((value) => value !== 0).length;
+  // Hago sumatoria de todas las horas y divido segun la cantidad de dias:
+  const average = trainned.reduce((pv, cv) => pv + cv) / periodLength;
+  const success = average >= target;
+  let rating = 1;
+  let ratingDescription = 'So low!';
+
+  if (average >= 1) {
+    rating = average >= 2.5 ? 3 : 2;
+    ratingDescription = average >= 2.5 ? 'You are On Fire!' : 'not too bad but could be better'; 
+  }
+
+  return { periodLength, trainingDays, target, average, success, rating, ratingDescription };
+}
+
+console.log(exerciseCalculator([3, 0, 2, 4.5, 0, 3, 1], 2));
+
