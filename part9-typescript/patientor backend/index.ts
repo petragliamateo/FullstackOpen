aa44d2a6065f896f4df57e1159e3bcbe0eafd67b
Mateo <petragliamateo@gmail.com>
2022-09-30 11:02:18 -0300

    9.9

--- part9-typescript/patientor backend/index.ts
+++ part9-typescript/patientor backend/index.ts
@@ -1,13 +1,15 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   res.send('pong')
 })
 
-const PORT = 3003
+const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
